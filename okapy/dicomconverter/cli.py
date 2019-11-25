42239ec68aa54d9815083e0e88fa2c46e1b3c3a8
Oreiller <valentin.oreiller@hevs.ch>
2019-11-25 10:31:46 +0100

    added the option to change the name of the output

--- okapy/dicomconverter/cli.py
+++ okapy/dicomconverter/cli.py
@@ -1,4 +1,5 @@
 import os
+from string import Template
 
 import click
 import logging
@@ -12,9 +13,10 @@ from okapy.dicomconverter.dicom_walker import DicomWalker
 @click.option('-o', '--output_filepath', required=True, type=click.Path())
 @click.option('-l', '--list_labels', default=None, type=str)
 @click.option('-e', '--extension', default='nii', type=str)
+@click.option('-n', '--name_output', default=None, type=str)
 #@click.option('-r', '--resampling_px_spacing', default=None,
 #              type=(float, float, float), required=False)
-def main(input_directory, output_filepath, list_labels, extension):
+def main(input_directory, output_filepath, list_labels, extension, name_output):
     """
     Convert to dicom to the right format based on extension
     """
@@ -26,6 +28,9 @@ def main(input_directory, output_filepath, list_labels, extension):
 
     walker = DicomWalker(input_directory, output_filepath,
                             list_labels=list_labels, extension_output=extension)
+    if name_output is not None:
+        walker.template_filename = Template(name_output + '_${modality}.${ext}')
+
     walker.walk()
     walker.fill_images()
     walker.convert()
