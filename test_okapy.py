1ed1b60c93bd462160437aa01cbf3c4166d13aa5
Roger Schaer <roger.schaer@hevs.ch>
2022-10-05 17:43:40 +0200

    Modify test for MR with submodalities

--- test_okapy.py
+++ test_okapy.py
@@ -2,12 +2,16 @@ from okapy.dicomconverter.converter import ExtractorConverter
 
 
 # okapy defaults - pyradiomics
-params = "/data/imagine-data/feature-presets-samples/PETCT_pyradiomics.yaml"
-folder = "/data/Datasets/Sample/Clark Kent - CLARKKENT"
+#params = "/data/imagine-data/feature-presets-samples/PETCT_pyradiomics.yaml"
+#folder = "/data/Datasets/Sample/Clark Kent - CLARKKENT"
 
 # imagine defaults - pyradiomics
 # params = "/data/imagine-data/feature-preset-samples/MR_all.yaml"
 # folder = "/data/imagine-data/brats-data/00834-568"
+
+# MRI imagine
+params = "/data/ownCloud/Projects/IMAGINE/FeaturePresets/qi_config_leomed.yaml"
+folder = "/data/Datasets/BRATS-MGMT-test/00008-6/"
 
 converter = ExtractorConverter.from_params(params)
 
